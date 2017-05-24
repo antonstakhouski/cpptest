2ea23735a7351cd8c620ce468214ff3d479f537e
Anton Stakhouski <antonstakhouski@gmail.com>
2017-05-25 00:20:41 +0300

    button handler moved to another class

--- cpptest/WidgetWithButton.cpp
+++ cpptest/WidgetWithButton.cpp
@@ -1,24 +1,17 @@
 #include "WidgetWithButton.h"
 #include "ui_WidgetWithButton.h"
-#include "movewidget.h"
-
 #include <iostream>
 
 using namespace std;
 
 WidgetWithButton::WidgetWithButton(QWidget *parent) :
-        QWidget(parent),
-        ui(new Ui::WidgetWithButton)
+    QWidget(parent),
+    ui(new Ui::WidgetWithButton)
 {
     setMouseTracking(true);
-    //    setFixedSize(size());
+    setFixedSize(size());
     ui->setupUi(this);
     ui->pushButton->installEventFilter(this);
-
-    btnHeight = ui->pushButton->height();
-    btnWidth = ui->pushButton->width();
-    trickyOffset = 10;
-
     connect(ui->pushButton, SIGNAL(clicked(bool)), this, SLOT(btnClicked()));
 }
 
@@ -32,202 +25,31 @@ void WidgetWithButton::btnClicked()
     cout << "You WIN!" << endl;
 }
 
-void WidgetWithButton::moveToDir(int btnX, int btnY, direction finalDir, int* res)
-{
-    MoveLeft moveLeft = MoveLeft(btnWidth, btnHeight, trickyOffset);
-    MoveRight moveRight(btnWidth, btnHeight, trickyOffset);
-    MoveUp moveUp(btnWidth, btnHeight, trickyOffset);
-    MoveDown moveDown(btnWidth, btnHeight, trickyOffset);
-    MoveTopLeft moveTopLeft(btnWidth, btnHeight, trickyOffset);
-    MoveTopRight moveTopRight(btnWidth, btnHeight, trickyOffset);
-    MoveBottomLeft moveBottomLeft(btnWidth, btnHeight, trickyOffset);
-    MoveBottomRight moveBottomRight(btnWidth, btnHeight, trickyOffset);
-    MoveNull moveNull(btnWidth, btnHeight, trickyOffset);
-    switch (finalDir) {
-    case LEFT:
-        moveLeft.move(btnX, btnY, res);
-        break;
-    case RIGHT:
-        moveRight.move(btnX, btnY, res);
-        break;
-    case UP:
-        moveUp.move(btnX, btnY, res);
-        break;
-    case DOWN:
-        moveDown.move(btnX, btnY, res);
-        break;
-    case TOPLEFT:
-        moveTopLeft.move(btnX, btnY, res);
-        break;
-    case TOPRIGHT:
-        moveTopRight.move(btnX, btnY, res);
-        break;
-    case BOTTOMLEFT:
-        moveBottomLeft.move(btnX, btnY, res);
-        break;
-    case BOTTOMRIGHT:
-        moveBottomRight.move(btnX, btnY, res);
-        break;
-    default:
-        moveNull.move(btnX, btnY, res);
-        break;
-    }
-}
-
-void WidgetWithButton::chooseDirSimple(int rx, int ry, int btnX, int btnY, direction* resDirect)
-{
-    direction xDir;
-    direction yDir;
-    if (rx <= btnWidth / 2){
-        if (btnX + 2 * btnWidth + trickyOffset >= widgetWidth) {
-            xDir = STAY;
-        } else {
-            xDir = RIGHT;
-        }
-    } else {
-        if (btnX - btnWidth  - trickyOffset <= widgetX) {
-            xDir = STAY;
-        } else {
-            xDir = LEFT;
-        }
-    }
-
-    if(ry <= btnHeight / 2) {
-        if (btnY + 2 * btnHeight  + trickyOffset >= widgetHeight){
-            yDir = STAY;
-        } else {
-            yDir = DOWN;
-        }
-    } else {
-        if (btnY - btnHeight  - trickyOffset <= widgetY) {
-            yDir = STAY;
-        } else {
-            yDir = UP;
-        }
-    }
-    resDirect[0] = xDir;
-    resDirect[1] = yDir;
-}
-
-void WidgetWithButton::chooseDestination(direction xDir, direction yDir,
-                                         int btnX, int btnY, direction* finalDir)
-{
-    *finalDir = STAY;
-    if (xDir == LEFT && yDir == STAY )
-        *finalDir = LEFT;
-    if (xDir == RIGHT && yDir == STAY )
-        *finalDir = RIGHT;
-    if (xDir == STAY && yDir == UP)
-        *finalDir = UP ;
-    if (xDir == STAY && yDir == DOWN)
-        *finalDir = DOWN;
-
-    if (xDir == LEFT && yDir == UP)
-        *finalDir = TOPLEFT;
-    if (xDir == RIGHT && yDir == UP)
-        *finalDir = TOPRIGHT;
-    if (xDir == LEFT && yDir == DOWN)
-        *finalDir = BOTTOMLEFT;
-    if (xDir == RIGHT && yDir == DOWN)
-        *finalDir = BOTTOMRIGHT;
-
-    if (xDir == STAY && yDir == STAY) {
-        if (btnX > widgetWidth / 2 && btnY < widgetHeight / 2) {
-            if (btnY + 2 * btnHeight + trickyOffset < widgetHeight) {
-                *finalDir = DOWN;
-            } else {
-                if (btnX - btnWidth - trickyOffset > widgetX) {
-                    *finalDir = LEFT;
-                }
-            }
-        } else {
-
-            if (btnX > widgetWidth / 2 && btnY > widgetHeight / 2) {
-                if (btnX - btnWidth - trickyOffset > widgetX) {
-                    *finalDir = LEFT;
-                } else {
-                    if (btnY - btnHeight - trickyOffset > widgetY) {
-                        *finalDir = UP;
-                    } else {
-                        if (btnX + 2 * btnWidth + trickyOffset < widgetWidth) {
-                            *finalDir = RIGHT;
-                        }
-                    }
-                }
-            } else {
-
-                if (btnX < widgetWidth / 2 && btnY > widgetHeight / 2) {
-                    if (btnY - btnHeight - trickyOffset > widgetY) {
-                        *finalDir = UP;
-                    } else {
-                        if (btnX + 2 * btnWidth + trickyOffset < widgetWidth) {
-                            *finalDir = RIGHT;
-                        }
-                    }
-                } else {
-
-                    if (btnX < widgetWidth / 2 && btnY < widgetHeight / 2) {
-                        if (btnX + 2 * btnWidth + trickyOffset < widgetWidth) {
-                            *finalDir = RIGHT;
-                        } else {
-                            if (btnY + 2 * btnHeight + trickyOffset < widgetHeight) {
-                                *finalDir = DOWN;
-                            } else{
-                                if (btnY - btnHeight - trickyOffset > widgetY) {
-                                    *finalDir = UP;
-                                }
-                            }
-                        }
-                    }
-                }
-            }
-        }
-    }
-}
-
 void WidgetWithButton::moveAway()
 {
     QPoint p = this->mapFromGlobal(QCursor::pos());
-
     int x = p.x();
     int y = p.y();
-
-    int btnX = ui->pushButton->x();
-    int btnY = ui->pushButton->y();
-
-    widgetHeight = this->size().height();
-    widgetWidth = this->size().width();
-    widgetX = this->mapFromGlobal(this->pos()).x();
-    widgetY = this->mapFromGlobal(this->pos()).y();
-
-    int rx = x - btnX;
-    int ry = y - btnY;
-
-    direction xDir = STAY;
-    direction yDir = STAY;
-
-    direction* resDirect = new direction[2];
-    chooseDirSimple(rx, ry, btnX, btnY, resDirect);
-    xDir = resDirect[0];
-    yDir = resDirect[1];
-
-    direction* finalDir = new direction;
-    chooseDestination(xDir, yDir, btnX, btnY, finalDir);
-
     int* res = new int[2];
-    moveToDir(btnX, btnY, *finalDir, res);
+    enterHandler.moveAway(x,
+                          y,
+                          ui->pushButton->x(),
+                          ui->pushButton->y(),
+                          ui->pushButton->width(),
+                          ui->pushButton->height(),
+                          this->mapFromGlobal(this->pos()).x(),
+                          this->mapFromGlobal(this->pos()).y(),
+                          this->size().width(),
+                          this->size().height(),
+                          res
+                          );
     ui->pushButton->move(res[0], res[1]);
-
-    delete(resDirect);
-    delete(res);
-    delete(finalDir);
 }
 
 bool WidgetWithButton::eventFilter(QObject *obj, QEvent *event)
 {
     if (obj == (QObject*)ui->pushButton) {
-        if (event->type() == QEvent::Enter)
-        {
+        if (event->type() == QEvent::Enter) {
             moveAway();
         }
     }
--- cpptest/WidgetWithButton.h
+++ cpptest/WidgetWithButton.h
@@ -3,6 +3,7 @@
 
 #include <QWidget>
 #include <QPushButton>
+#include "enterhandler.h"
 
 namespace Ui {
 class WidgetWithButton;
@@ -12,6 +13,8 @@ class WidgetWithButton : public QWidget
 {
     Q_OBJECT
 
+private:
+    EnterHandler enterHandler;
 public:
     explicit WidgetWithButton(QWidget *parent = 0);
     ~WidgetWithButton();
@@ -30,29 +33,6 @@ Q_SIGNALS:
 
 private:
     Ui::WidgetWithButton *ui;
-
-    enum direction { STAY, LEFT, RIGHT, UP, DOWN, BOTTOMLEFT, BOTTOMRIGHT, TOPLEFT, TOPRIGHT};
-    int btnWidth;
-    int btnHeight;
-    int trickyOffset;
-    int widgetHeight;
-    int widgetWidth;
-    int widgetX;
-    int widgetY;
-
-    void moveToDir(int btnX, int btnY, direction finalDir, int* res);
-    void chooseDirSimple(int rx, int ry, int btnX, int btnY, direction *resDirect);
-    void chooseDestination(direction xDir, direction yDir, int btnX, int btnY, direction* finalDir);
-
-    void moveLeft(int btnX, int btnY, int *res);
-    void moveRight(int btnX, int btnY, int *res);
-    void moveUp(int btnX, int btnY, int *res);
-    void moveDown(int btnX, int btnY, int *res);
-    void moveTopLeft(int btnX, int btnY, int *res);
-    void moveTopRight(int btnX, int btnY, int *res);
-    void moveBottomLeft(int btnX, int btnY, int *res);
-    void moveBottomRight(int btnX, int btnY, int *res);
-    void moveNull(int btnX, int btnY, int *res);
 };
 
 #endif // WIDGETWITHBUTTON_H
